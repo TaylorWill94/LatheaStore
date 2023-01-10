3c7ebec7305bea57a258d20b55f0be6e5639c6e1
Taylor W <taylorjwill09@gmail.com>
2023-01-10 18:07:46 -0500

    Updates shop page and home page

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -2,10 +2,11 @@ import "./Shop.css";
 
 export const Shop = () => {
   return (
-    <div className="shop">
-      <div className="shop-section">
-        <h3>Shop</h3>
+    <div className="shop-wrapper">
+      <div className="shop-heading">
+        <h3>Shop The Collection</h3>
       </div>
+      <div className="shop-section">Clothing</div>
     </div>
   );
 };
