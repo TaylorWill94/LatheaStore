c0bb4fe6972f1e1e87d51482c483f45c6a637122
Taylor W <taylorjwill09@gmail.com>
2023-01-09 23:06:44 -0500

    Creates home and nav section

---
+++ src/components/home/Home.js
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div>
+      <h1>Lathea Knight Store</h1>
+    </div>
+  );
+};
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+export const Nav = () => {
+  return (
+    <nav className="nav">
+      <Link to="/">
+        <h3 className="nav-item">Home</h3>
+      </Link>
+      <h3 className="nav-item">Shop</h3>
+      <h3 className="nav-item">About</h3>
+    </nav>
+  );
+};
