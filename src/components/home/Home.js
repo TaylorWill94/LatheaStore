6e9a401add986eced4535903d23f0662e058bf96
Taylor W <taylorjwill09@gmail.com>
2023-01-10 17:29:26 -0500

    Makes styling changes to the home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,27 @@
+import "./Home.css";
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Lathea Knight Store</h1>
+    <div className="home-wrapper">
+      <section className="website-title">
+        <h1 className="store-title">Lathea Knight Store</h1>
+      </section>
+      <div className="home-sec">
+        <section className="website-info">
+          <div>
+            {" "}
+            <p>sweet to the soul</p>
+          </div>
+          <div>
+            {" "}
+            <h2 className="exclusives-title">Exclusive</h2>
+          </div>
+          <div>
+            {" "}
+            <h3>ShOP NOW --></h3>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
