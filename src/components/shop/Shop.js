2010b510b1bbf35c718cb946d8d47e8b8d396946
Taylor W <taylorjwill09@gmail.com>
2023-01-13 15:54:32 -0500

    Edits shop page

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,4 +1,5 @@
 import "./Shop.css";
+import product from "./praynotpreyapparelsweater.webp";
 
 export const Shop = () => {
   return (
@@ -6,7 +7,10 @@ export const Shop = () => {
       <div className="shop-heading">
         <h3>Shop The Collection</h3>
       </div>
-      <div className="shop-section">Clothing</div>
+      <div className="shop-section">
+        <h1 style={{ textAlign: "center" }}>Clothing</h1>
+        <img src={product} alt="test-product" width="500" />
+      </div>
     </div>
   );
 };
