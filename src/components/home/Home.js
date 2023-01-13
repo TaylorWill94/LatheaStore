a119c998b35bcdd7a7d8a0341cec8c91217bd6b2
Taylor W <taylorjwill09@gmail.com>
2023-01-12 22:39:34 -0500

    Creates styles for tablet and mobile

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,22 +1,28 @@
 import "./Home.css";
+import testPhoto from "./test-photo.jpeg";
 
 export const Home = () => {
   return (
-    <div className="home-wrapper">
+    <div>
       <section className="website-title">
         <h1 className="store-title">Lathea Knight Store</h1>
       </section>
-      <div className="home-sec">
-        <section className="website-info">
-          <div>
-            <p>sweet to the soul</p>
-          </div>
-          <div>
-            <h2 className="exclusives-title">Exclusive</h2>
-          </div>
-          <div>
-            <h3>SHOP NOW</h3>
-          </div>
+      <div className="home-wrapper">
+        <div className="info-sec">
+          <section className="website-info">
+            <div>
+              <p>sweet to the soul</p>
+            </div>
+            <div>
+              <h2 className="exclusives-title">Exclusive</h2>
+            </div>
+            <div>
+              <h3>SHOP NOW</h3>
+            </div>
+          </section>
+        </div>
+        <section className="img-section">
+          <img src={testPhoto} alt="fashion" width={500} />
         </section>
       </div>
     </div>
