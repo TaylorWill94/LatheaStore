c1ddbfa16e1ad5a3f4e6727048a5ccb1898b92ab
Taylor W <taylorjwill09@gmail.com>
2023-01-13 15:50:59 -0500

    Makes changes to homepage

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -29,26 +29,6 @@ export const Home = () => {
         </div>
       </div>
       <Soul />
-      <div>
-        <div className="home-wrapper">
-          <div className="info-sec">
-            <section className="website-info">
-              <div>
-                <p>sweet to the soul</p>
-              </div>
-              <div>
-                <h2 className="exclusives-title">Exclusive</h2>
-              </div>
-              <div>
-                <h3>SHOP NOW</h3>
-              </div>
-            </section>
-          </div>
-          <section className="img-section">
-            <img src={testPhoto} alt="fashion" width={500} class="center" />
-          </section>
-        </div>
-      </div>
     </>
   );
 };
--- src/components/home/Soul.js
+++ src/components/home/Soul.js
@@ -1,3 +1,26 @@
+import testPhoto2 from "./test-photo-2.jpeg";
+
 export const Soul = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="home-wrapper">
+        <div className="info-sec">
+          <section className="website-info">
+            <div>
+              <p>sweet to the soul</p>
+            </div>
+            <div>
+              <h2 className="exclusives-title">Exclusive</h2>
+            </div>
+            <div>
+              <h3>SHOP NOW</h3>
+            </div>
+          </section>
+        </div>
+        <section className="img-section">
+          <img src={testPhoto2} alt="fashion" width={500} class="center" />
+        </section>
+      </div>
+    </div>
+  );
 };
